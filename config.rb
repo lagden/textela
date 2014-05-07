0af0e3e20b56f825a701187f43154bb38c1471d8
Thiago Lagden <lagden@gmail.com>
2014-05-06 21:45:02 -0300

    tela de cotacao

---
+++ config.rb
@@ -0,0 +1,12 @@
+require 'bootstrap-sass'
+
+http_path = "/"
+css_dir = "dev/css"
+sass_dir = "sass"
+images_dir = "dev/images"
+javascripts_dir = "dev/js"
+fonts_dir = "dev/fonts"
+output_style = :expanded
+preferred_syntax = :sass
+relative_assets = true
+line_comments = true
